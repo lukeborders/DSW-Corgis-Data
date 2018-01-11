2c108a6ab4226577466d47a0625d586ba0005e1b
Luke Borders <31492762+lukeborders@users.noreply.github.com>
2018-01-11 09:42:37 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -17,18 +17,41 @@ def get_options():
     return options 
     #logang 
    
-def get_numberz(name):   
+def get_numberz(name,year):   
     with open('health.json') as health_data:
         data = json.load(health_data)
     diseaseDict = {}
     fact = 0
     for n in data:
-        diseaseDict[str(n["disease"])] = n["number"]
+        if name == n["disease"] and str(n["year"]) == year:
+            fact += n["number"]
+    print(year)
+    print(name)
+    print(fact)
     return fact
-            
+ 
+def get_options2(fact):
+    with open('health.json') as health_data:
+        data = json.load(health_data)
+    options=""
+    dis = "MEASLES"
+    for t in data:
+        #dank if statement
+        if t["disease"] not in dis:
+            options += Markup("<option value=" + '"' +t["disease"] + '"'+">" + t["disease"] + "</option>")
+        dis = t["disease"] 
+        if dis == t["disease"]:
+            fact += n["number"]
+    return options
+    #logang
+ 
 @app.route("/")
 def render_main():
     return render_template('index.html')
+
+@app.route("/U")
+def render_U():
+    return render_template('U.html',yearOpt = get_options(), diseaseOpt = get_options2())
 
 @app.route("/statistics")
 def render_stats():
@@ -37,7 +60,12 @@ def render_stats():
 @app.route("/selection", methods = ['GET','POST'])
 def render_chart():
     year = request.args['years']
-    return render_template('statistics.html', years = get_options(), measles = get_numberz("MEASLES"), polio = get_numberz("POLIO"), smallpox = get_numberz("SMALLPOX"), pertussis = get_numberz("PERTUSSIS"), hepatitusa = get_numberz("HEPATITUS A"), rubella = get_numberz("RUBELLA"), mumps = get_numberz("MUMPS"))   
+    return render_template('statistics.html', years = get_options(), measles = get_numberz("MEASLES",year), polio = get_numberz("POLIO",year), smallpox = get_numberz("SMALLPOX",year), pertussis = get_numberz("PERTUSSIS",year), hepatitusa = get_numberz("HEPATITUS A",year), rubella = get_numberz("RUBELLA",year), mumps = get_numberz("MUMPS",year))   
+ 
+@app.route("/yes", methods = ['GET','POST'])
+def render_jfact():
+    year = request.args['year']
+    return render_template("U.html",yearOpt = get_options(), diseaseOpt = get_options2())
  
 if __name__ =="__main__":
     app.run(debug=True, port=5000)
